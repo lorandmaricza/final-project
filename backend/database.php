40b5dd8c1db845b3e6164aea9f79cd6e17f4390b
Lorand Maricza <maricza.lorand@gmail.com>
2023-10-27 12:29:32 +0200

    connect to google cloud SQL database

--- backend/database.php
+++ backend/database.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
 
-$DATABASE_HOST = '127.0.0.1';
+$DATABASE_HOST = '35.194.48.24';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = 'root';
-$DATABASE_NAME = 'project_db';
-$DATABASE_PORT = 8889;
+$DATABASE_NAME = 'project-db';
+//$DATABASE_PORT = 8889;
 $conn = mysqli_connect(
     $DATABASE_HOST,
     $DATABASE_USER,
     $DATABASE_PASS,
-    $DATABASE_NAME,
-    $DATABASE_PORT
+    $DATABASE_NAME
 );
 
 error_reporting(E_ALL);
