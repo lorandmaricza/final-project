f3cdf65ca1ee8c446354ecc8a86d6358306c98a6
Lorand Maricza <maricza.lorand@gmail.com>
2023-11-06 18:11:36 +0100

    connect securely to google cloud SQL database

--- backend/database.php
+++ backend/database.php
@@ -1,22 +1,6 @@
 <?php
 session_start();
-
-$DATABASE_HOST = '35.194.48.24';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'root';
-$DATABASE_NAME = 'project-db';
-//$DATABASE_PORT = 8889;
-$conn = mysqli_connect(
-    $DATABASE_HOST,
-    $DATABASE_USER,
-    $DATABASE_PASS,
-    $DATABASE_NAME
-);
-
 error_reporting(E_ALL);
 
-if (!$conn) {
-    die('Could not connect to the database: ' .
-        mysqli_error($conn));
-}
+$database = new PDO("mysql:host=".getenv('DATABASE_HOST').";dbname=".getenv('DATABASE_NAME'), getenv('DATABASE_USER'), getenv('DATABASE_PASS'));
 
